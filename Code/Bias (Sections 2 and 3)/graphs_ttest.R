46e7daa00f8f896c58492ba4fc8db936beac6891
Nikolaus <nschaefer1211@gmail.com>
2019-06-18 19:30:55 +0200

    graphs completed 1

--- Code/Bias (Sections 2 and 3)/graphs_ttest.R
+++ Code/Bias (Sections 2 and 3)/graphs_ttest.R
@@ -14,11 +14,13 @@ png(filename="bias_corr2.png",
     height=5, 
     pointsize=12, 
     res=250)
-plot(Shooter, ttest2_sort$adj_diff * 100,  ylim = c(-100, 100), cex = 1.5, type = "p", pch = 16, col = "black")
+plot(Shooter, ttest2_sort$adj_diff * 100, ylab = "Bias-corrected difference (percentage points)",  ylim = c(-90, 95),
+     cex = 1.5, type = "p", pch = 16, col = "black")
 segments(x0 = Shooter, x1 = Shooter, y0 = ttest2_sort$adj_diff* 100 - ttest2_sort$se2 * 100,
-         y1 = ttest2_sort$adj_diff * 100 + ttest2_sort$se2 * 100, lwd = 3, col = "gray60")
+         y1 = ttest2_sort$adj_diff * 100 + ttest2_sort$se2 * 100, lwd = 3.5, col = "gray60")
 segments(x0 = Shooter, x1 = Shooter, y0 = ttest2_sort$CI_lower_bound * 100, y1 = ttest2_sort$CI_upper_bound * 100, lwd = 1, col = "black")
-legend(20,-90,c("+/- Std. Err.", "95 % CI"), col=c("gray60","black"), horiz = TRUE)
+lines(x, rep(0, 100), type = "l")
+legend(7,-65,c("+/- Std. Err.", "95 % CI"), col=c("gray60","black"), horiz = TRUE, lwd = c(3.5, 1), lty = c(1, 1))
 dev.off()
 
 
@@ -36,11 +38,13 @@ png(filename="bias_corr3.png",
     height=5, 
     pointsize=12, 
     res=250)
-plot(Shooter, ttest3_sort$adj_diff[-26] * 100,  ylim = c(-100, 100), cex = 1.5, type = "p", pch = 16, col = "black")
+plot(Shooter, ttest3_sort$adj_diff[-26] * 100, ylab = "Bias-corrected difference (percentage points)",  ylim = c(-100, 100),
+     cex = 1.5, type = "p", pch = 16, col = "black")
 segments(x0 = Shooter, x1 = Shooter, y0 = ttest3_sort$adj_diff* 100 - ttest3_sort$se3 * 100,
-         y1 = ttest3_sort$adj_diff * 100 + ttest3_sort$se3 * 100, lwd = 3, col = "gray60")
+         y1 = ttest3_sort$adj_diff * 100 + ttest3_sort$se3 * 100, lwd = 3.5, col = "gray60")
 segments(x0 = Shooter, x1 = Shooter, y0 = ttest3_sort$CI_lower_bound * 100, y1 = ttest3_sort$CI_upper_bound * 100, lwd = 1, col = "black")
-legend(20,-90,c("+/- Std. Err.", "95 % CI"), col=c("gray60","black"), horiz = TRUE)
+lines(x, rep(0, 100), type = "l")
+legend(7,-65,c("+/- Std. Err.", "95 % CI"), col=c("gray60","black"), horiz = TRUE, lwd = c(3.5, 1), lty = c(1, 1))
 dev.off()
 
 
@@ -58,11 +62,13 @@ png(filename="bias_corr4.png",
     height=5, 
     pointsize=12, 
     res=250)
-plot(Shooter, ttest4_sort$adj_diff[-c(2,26)] * 100,  ylim = c(-100, 100), cex = 1.5, type = "p", pch = 16, col = "black")
+plot(Shooter, ttest4_sort$adj_diff[-c(2,26)] * 100, ylab = "Bias-corrected difference (percentage points)", xlim = c(0,25), ylim = c(-150, 165),
+     cex = 1.5, type = "p", pch = 16, col = "black")
 segments(x0 = Shooter, x1 = Shooter, y0 = ttest4_sort$adj_diff* 100 - ttest4_sort$se4 * 100,
          y1 = ttest4_sort$adj_diff * 100 + ttest4_sort$se4 * 100, lwd = 3, col = "gray60")
 segments(x0 = Shooter, x1 = Shooter, y0 = ttest4_sort$CI_lower_bound * 100, y1 = ttest4_sort$CI_upper_bound * 100, lwd = 1, col = "black")
-legend(20,-90,c("+/- Std. Err.", "95 % CI"), col=c("gray60","black"), horiz = TRUE)
+lines(x, rep(0, 100), type = "l")
+legend(7,-90,c("+/- Std. Err.", "95 % CI"), col=c("gray60","black"), horiz = TRUE, lwd = c(3.5, 1), lty = c(1, 1))
 dev.off()
 
 
