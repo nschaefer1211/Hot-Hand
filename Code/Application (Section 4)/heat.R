f471690f03a1bda208325b176a08b44d637d8cc6
Nikolaus <nschaefer1211@gmail.com>
2019-06-21 01:16:07 +0200

    update application part

---
+++ Code/Application (Section 4)/heat.R
@@ -0,0 +1,59 @@
+# Actual % over past n shots - Expected % over past n shots
+
+load("modelData.Rdata")
+
+model <- test
+model$id <- 1:dim(model)[1]
+
+games <- unique(test$game_id)
+heat <- c()
+id <- c()
+for(j in 1:length(games)){
+  game_j <- model %>%
+    filter(game_id == games[j]) 
+  teams <- unique(game_j$team)
+  for(i in 1:length(teams)){
+    game_j_team_i <- game_j %>%
+      filter(team == teams[i])
+    heat <- c(heat, rep(NA, 4))
+    id <- c(id, game_j_team_i$id[1:4])
+    for(k in 5:dim(game_j_team_i)[1]){
+      #actual pct of made shots in the past four shots
+      actual_pct <- sum(game_j_team_i$result[(k-4):(k-1)])/4
+      #expected pct of past four shots
+      exp_pct <- sum((game_j_team_i$pred[(k-4):(k-1)]))/4
+      heat <- c(heat, (actual_pct - exp_pct))
+      id <- c(id, game_j_team_i$id[k])
+    }
+    
+  }
+}
+
+temp <- data.frame(id, heat)
+temp <- temp[order(temp$id),]
+model$heat <- temp$heat
+
+summary(lm(actual ~ heat + pred, data = na.omit(model)))
+
+
+# apply the found model on all the data
+red <- data %>%
+  select(-game_id, -date, -away_score, -home_score, -points, -event_type, -player) %>%
+  mutate(shot_type = as.factor(shot_type), shot_diff = as.factor(shot_diff), team = as.factor(team))
+
+OLS <- glm(result ~., family = binomial, data = red)
+shot_pred <- round(predict(fit, newdata = data, type = "response", digits = 2))
+shot_pred <- ifelse(shot_pred <= 0, 0.01, shot_pred)
+shot_pred <- ifelse(shot_pred >= 1, 0.99, shot_pred)
+data$shot_pred <- shot_pred
+
+
+data$actual <- rep(NA, length(unique(data$shot_pred)))
+e_prob <- sort(unique(test$pred))
+for(i in 1:length(e_prob)){
+  temp1 <- test %>% filter(pred == e_prob[i], result == 1)
+  temp2 <- test %>% filter(pred == e_prob[i], result == 0)
+  actual[i] <- length(temp1$result)/(length(temp1$result) + length(temp2$result))
+}
+
+
--- Code/Application (Section 4)/model2.R
+++ Code/Application (Section 4)/model2.R
@@ -1,12 +1,17 @@
 ##############
 
 load("pbpData.Rdata")
+"%!in%" <- function(x, y) !("%in%"(x, y))
+library(dplyr)
+
+
 
 #creating additional variables
 data <- Viewdata %>%
   mutate(period = as.factor(period), shot_distance2 = shot_distance^2, shot_distance3 = shot_distance^3,
          score_diff = abs(home_score - away_score)) %>%
   select(-description)
+
 
 
 data$forced <- ifelse(data$remaining_time <= 2, 1, 0)
@@ -44,18 +49,22 @@ data$result <- ifelse(data$result == "made", 1, 0)
 ############################################ ANALYSIS #####################################################
 ###########################################################################################################
 
-#split into train and test data
-smp_size <- floor(0.5* nrow(data))
+#split into train and test data 50:50 split
+#half the games serve as a training set 
+smp_size <- floor(0.5 * length(unique(data$game_id)))
 
 ## set the seed to make your partition reproducible
-set.seed(122)
-train_ind <- sample(seq_len(nrow(data)), size = smp_size)
 
-train <- data[train_ind, ] %>%
-  select(-game_id, -date, -away_score, -home_score, -points, -event_type, -player) %>%
+set.seed(176)
+train_ind <- sample(unique(data$game_id), size = smp_size)
+
+train <- data%>%
+  filter(game_id %in% train_ind) %>%
+  select(-date, -away_score, -home_score, -points, -event_type, -player) %>%
   mutate(shot_type = as.factor(shot_type), shot_diff = as.factor(shot_diff), team = as.factor(team))
-test <- data[-train_ind, ] %>%
-  select(-game_id, -date, -away_score, -home_score, -points, -player) %>%
+test <- data %>%
+  filter(game_id %!in% train_ind) %>%
+  select(-date, -away_score, -home_score, -points, -player) %>%
   mutate(shot_type = as.factor(shot_type), shot_diff = as.factor(shot_diff), team = as.factor(team))
 
 #null<- glm(result ~ 1, family = binomial (link = "cloglog"), data = train)
@@ -66,7 +75,7 @@ test <- data[-train_ind, ] %>%
 #both <- step(null, scope = list(upper=full), data=train, direction="both")
 
 
-fit <- glm(result ~ ., family = binomial, data = train)
+fit <- glm(result ~ . -game_id, family = binomial, data = train)
 pred <- round(predict(fit, newdata = test, type = "response"), digits = 2)
 
 pred <- ifelse(pred <= 0, 0.01, pred)
@@ -77,9 +86,29 @@ e_prob <- sort(unique(test$pred))
 for(i in 1:length(e_prob)){
   temp1 <- test %>% filter(pred == e_prob[i], result == 1)
   temp2 <- test %>% filter(pred == e_prob[i], result == 0)
-  actual[i] <- length(temp1$result)/(length(temp1$result) + length(temp2$result))
+  actual[i] <- round(length(temp1$result)/(length(temp1$result) + length(temp2$result)), digits = 2)
 }
 
+
 sum(abs(actual - e_prob))
-plot(actual, e_prob)
+plot(e_prob, actual)
 lines(seq(0, 1, length.out = 200), seq(0, 1, length.out = 200), col = "red")
+
+test$actual <- rep(NA, dim(test)[1])
+for(i in 1:dim(test)[1]){
+  for(j in 1:length(e_prob)){
+    if(test$pred[i] == e_prob[j]){
+      test$actual[i] <- actual[j]
+    }
+    else{
+      next
+    }
+  }
+}
+  
+summary(lm(actual~pred, data = test))
+
+
+
+
+save(test, file = "modelData.Rdata")
